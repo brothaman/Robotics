3d383f7af3f29b86289a34fc5206c637105cc0bb
Robert Brothers <rbrothers12@gmail.com>
2014-10-20 16:43:20 -0500

    getting all the functions used in robotics into to one file

---
+++ python/HW6/HW7.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import numpy as np, sympy as sy
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def end_effector( list_of_links):
+	number_of_links = len(list_of_links)
+	whatami = 0
+	for val in list_of_links:
+		for thing in val:
+			if str(type(thing)) == "<class 'sympy.core.symbol.Symbol'>":
+				whatami = 1
+	if whatami == 0:
+		A_list = np.array([ A_matrix(val) for val in list_of_links])
+	else:
+		A_list = np.array([ A_sym_matrix(val) for val in list_of_links])
+		print len(A_list)
+	val = np.matrix(np.identity(4))
+	EOL_A = []
+	i = 0
+	# pack a list
+	for i in A_list:
+		val = val*i
+		EOL_A.append(val[:,3][:3])
+		i = i + 1
+	return val, EOL_A
+
+def A_matrix(link_list):
+	[a, al, d, th] = link_list
+	A_mat = np.matrix((
+		(np.cos(th), -np.sin(th)*np.cos(al), np.sin(th)*np.sin(al), a*np.cos(th)), 
+		(np.sin(th),  np.cos(th)*np.cos(al),-np.cos(th)*np.sin(al), a*np.sin(th)), 
+		(0         ,  np.sin(al)           , np.cos(al)           , d           ), 
+		(0         ,  0                    , 0                    , 1           ), 
+		))
+	return A_mat
+
+def A_sym_matrix(link_list):
+	[a, al, d, th] = link_list
+	A_mat = np.matrix((
+		(sy.cos(th), -sy.sin(th)*sy.cos(al), sy.sin(th)*sy.sin(al), a*sy.cos(th)), 
+		(sy.sin(th),  sy.cos(th)*sy.cos(al),-sy.cos(th)*sy.sin(al), a*sy.sin(th)), 
+		(0         ,  sy.sin(al)           , sy.cos(al)           , d           ), 
+		(0         ,  0                    , 0                    , 1           ), 
+		))
+	return A_mat
+
+if __name__ == "__main__":
+	[th1,th2,th3,th5,th6,d3] = [np.pi/4, np.pi/4, 0, np.pi/4, np.pi/4, np.pi/4]
+	dh_mat = [  
+			[0,-np.pi/2, 0, th1],
+			[0, np.pi/2,.5, th2],
+			[0, 0, d3, 0],       
+			[0,-np.pi/2, 0, th3],
+			[0, np.pi/2, 0, th5],
+			[0, 0, .5, th6]      
+			]                    
+	stuff = end_effector(dh_mat)
+	A06 = stuff[0]
+	end_of_links = stuff[1]
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	X = np.array([ np.array(val[0])[0][0] for val in end_of_links])
+	Y = np.array([ np.array(val[1])[0][0] for val in end_of_links])
+	Z = np.array([ np.array(val[2])[0][0] for val in end_of_links])
+	print "The position of the End Effector is"
+	print A06[:,3][:3]
+	print "The orientation of the End Effector is"
+	print A06[:3,:3]
+	# make graph
+	ax.plot(X,Y,Z)
+	plt.show()
+	Axes3D.plot()
+	ax.savefig('plot1.png')
+
+
+'''
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+'''
---
+++ python/robotics_functions.py
@@ -0,0 +1,105 @@
+import numpy as np, sympy as sp
+
+#define numerical rotation matricies
+def rotation_z(theta):
+	return np.matrix([
+		[np.cos(theta), -np.sin(theta), 0],
+		[np.sin(theta),  np.cos(theta), 0],
+		[0, 0, 1]
+		])
+def rotation_y(theta):
+	return np.matrix([
+		[np.cos(theta), 0, -np.sin(theta)],
+		[0, 1, 0],
+		[np.sin(theta), 0,  np.cos(theta)]
+		])
+def rotation_x(theta):
+	return np.matrix([
+		[1, 0, 0],
+		[0, np.cos(theta), -np.sin(theta)],
+		[0, np.sin(theta),  np.cos(theta)]
+		])
+
+# define symbolic rotation matricies
+def sym_rotation_z(theta):
+	return sy.Matrix([
+		[sy.cos(theta), -sy.sin(theta), 0],
+		[sy.sin(theta),  sy.cos(theta), 0],
+		[0, 0, 1]
+		])
+def sym_rotation_y(theta):
+	return sy.Matrix([
+		[sy.cos(theta), 0, -sy.sin(theta)],
+		[0, 1, 0],
+		[sy.sin(theta), 0,  sy.cos(theta)]
+		])
+def sym_rotation_x(theta):
+	return sy.Matrix([
+		[1, 0, 0],
+		[0, sy.cos(theta), -sy.sin(theta)],
+		[0, sy.sin(theta),  sy.cos(theta)]
+		])
+
+# define numerical translation matricies
+def translation_z( d):
+	return np.matrix([
+		[1,0,0,0],
+		[0,1,0,0],
+		[0,0,1,d],
+		[0,0,0,1]
+		])
+def translation_y( d):
+	return np.matrix([
+		[1,0,0,0],
+		[0,1,0,d],
+		[0,0,1,0],
+		[0,0,0,1]
+		])
+def translation_x( d):
+	return np.matrix([
+		[1,0,0,d],
+		[0,1,0,0],
+		[0,0,1,0],
+		[0,0,0,1]
+		])
+
+# define symbolic translation matrices
+def sym_translation_z( d):
+	return sy.Matrix([
+		[1,0,0,0],
+		[0,1,0,0],
+		[0,0,1,d],
+		[0,0,0,1]
+		])
+def sym_translation_y( d):
+	return sy.Matrix([
+		[1,0,0,0],
+		[0,1,0,d],
+		[0,0,1,0],
+		[0,0,0,1]
+		])
+def sym_translation_x( d):
+	return sy.Matrix([
+		[1,0,0,d],
+		[0,1,0,0],
+		[0,0,1,0],
+		[0,0,0,1]
+		])
+# numerically convert 3x3 rotation to 4x4 rotation
+def convert3x3to4x4( matrix):
+	# add column of zeroes
+	matrix = np.hstack((matrix, np.transpose([np.zeros(3)])))
+	# add row of 0,0,0,1
+	matrix = np.vstack((matrix, np.array([0,0,0,1])))
+	return matrix
+# symbolically convert 3x3 rotation to 4x4 rotation
+def syms_convert3x3to4x4( matrix):
+	# add column of zeroes
+	matrix = sy.Matrix.hstack(matrix, sy.Matrix(sy.zeros(3))
+	# add row of 0,0,0,1
+	matrix = np.vstack((matrix, sy.Matrix([0,0,0,1]).T))
+	return matrix
+
+def denavait_hartenberg( link_list):
+		
+
