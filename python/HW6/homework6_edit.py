498ec97ace312ca7bf2ecefb4c0d100627a1b1fd
Robert Brothers <rbrothers12@gmail.com>
2014-11-01 02:04:25 -0500

    been upto quite a bit

--- python/HW6/homework6_edit.py
+++ python/HW6/homework6_edit.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-
+import warnings
+warnings.filterwarnings('ignore', 'The iteration is not making good progress')
 import sys
 sys.path.append(r"/Users/robertbrothers/Desktop/Fall 2014/Fundamentals_of_Robotics/robo_git/python/")
 import robotics_functions as rf
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy as sc
+from scipy import optimize
 import sympy as sy
 from mpl_toolkits.mplot3d import Axes3D
-
 
 #########################		Problem 1 #######################################
 # set up the D-H table
@@ -23,12 +25,15 @@ dh_table = np.array( [
 A0n = []
 A06 = np.identity(4)
 for link in dh_table:
+	print link
 	A06 = A06*rf.denavit_hartenberg(link)
+	print A06
 	A0n.append(A06)
 P = []
 for i in A0n:
 	P.append(i[:,3][:3])
 
+print A06
 X,Y,Z = [np.array([ p[i] for p in P]).T[0][0] for i in range(3)]
 
 # Plot the Graph
@@ -40,18 +45,48 @@ line3 = ax.plot([X[2],X[3]], [Y[2], Y[3]], [Z[2], Z[3]], 'r')
 line4 = ax.plot([X[3],X[4]], [Y[3], Y[4]], [Z[3], Z[4]], 'r')
 line5 = ax.plot([X[4],X[5]], [Y[4], Y[5]], [Z[4], Z[5]], 'r')
 plt.show()
+
 ######################		End of Problem1 ####################################
 
 #######################		Problem 2 #########################################
-[th1, th2, th3] = sy.symbols("th1 th2 th3")
+ee_position = np.array([.75,0,0])
+def inv_kin(th):
+	print th
+	dh_table = np.array([
+		[0.5, 0, 0, th[0]],
+		[0.5, 0, 0, th[1]],
+		[0.25,0, 0, th[2]]
+		])
+	A03 = np.identity(4)
+	for link in dh_table:
+		A03 = A03*rf.denavit_hartenberg(link)
+	x = A03[:,3][:3][0] - ee_position[0]
+	y = A03[:,3][:3][1] - ee_position[1]
+	z = A03[:,3][:3][2] - ee_position[2]
+	return np.array(np.hstack((x[0],y[0],z[0])))[0]
+
+th = sc.optimize.fsolve(inv_kin, [-.9, 1.2, 1.3], xtol=1e-3 )
 dh_table = np.array([
-	[0.5, 0, 0, -th1],
-	[0.5, 0, 0, th2],
-	[0.25,0, 0, th3]
+	[0.5, 0, 0, th[0]],
+	[0.5, 0, 0, th[1]],
+	[0.25,0, 0, th[2]]
 	])
-A0n
+A0n = []
 A03 = np.identity(4)
 for link in dh_table:
-	A03 = A03*rf.sym_denavit_hartenberg(link)
-	A0n.append(A06)
-ee_position = np.array([[.75],[0],[0]])
+	A03 = A03*rf.denavit_hartenberg(link)
+	A0n.append(A03)
+# plot the graph
+
+P = []
+for i in A0n:
+	P.append(i[:,3][:3])
+
+print A03
+X,Y,Z = [np.array([ p[i] for p in P]).T[0][0] for i in range(3)]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+linei = ax.plot([   0,X[0]], [   0, Y[0]], [   0, Z[0]], 'r')
+line1 = ax.plot([X[0],X[1]], [Y[0], Y[1]], [Z[0], Z[1]], 'r')
+line2 = ax.plot([X[1],X[2]], [Y[1], Y[2]], [Z[1], Z[2]], 'r')
+plt.show()
--- python/robotics_functions.py
+++ python/robotics_functions.py
@@ -3,15 +3,15 @@ import numpy as np, sympy as sy
 #define numerical rotation matricies
 def rotation_z(theta):
 	return np.matrix([
-		[np.cos(theta), -np.sin(theta), 0],
-		[np.sin(theta),  np.cos(theta), 0],
+		[ np.cos(theta), -np.sin(theta), 0],
+		[ np.sin(theta),  np.cos(theta), 0],
 		[0, 0, 1]
 		])
 def rotation_y(theta):
 	return np.matrix([
-		[np.cos(theta), 0, -np.sin(theta)],
+		[ np.cos(theta), 0,  np.sin(theta)],
 		[0, 1, 0],
-		[np.sin(theta), 0,  np.cos(theta)]
+		[-np.sin(theta), 0,  np.cos(theta)]
 		])
 def rotation_x(theta):
 	return np.matrix([
@@ -23,15 +23,15 @@ def rotation_x(theta):
 # define symbolic rotation matricies
 def sym_rotation_z(theta):
 	return sy.Matrix([
-		[sy.cos(theta), -sy.sin(theta), 0],
-		[sy.sin(theta),  sy.cos(theta), 0],
+		[ sy.cos(theta), -sy.sin(theta), 0],
+		[ sy.sin(theta),  sy.cos(theta), 0],
 		[0, 0, 1]
 		])
 def sym_rotation_y(theta):
 	return sy.Matrix([
-		[sy.cos(theta), 0, -sy.sin(theta)],
+		[ sy.cos(theta), 0,  sy.sin(theta)],
 		[0, 1, 0],
-		[sy.sin(theta), 0,  sy.cos(theta)]
+		[-sy.sin(theta), 0,  sy.cos(theta)]
 		])
 def sym_rotation_x(theta):
 	return sy.Matrix([
@@ -100,15 +100,88 @@ def syms_convert3x3to4x4( matrix):
 	matrix = sy.Matrix.vstack((matrix, sy.Matrix([0,0,0,1]).T))
 	return matrix
 
-def denavit_hartenberg( link_list):
+def denavit_hartenberg( link):
 	return (
-		translation_z(link_list[0])*convert3x3to4x4(rotation_z(link_list[1]))*
-		translation_x(link_list[2])*convert3x3to4x4(rotation_x(link_list[3]))
+		translation_z(link[2])*convert3x3to4x4(rotation_z(link[3]))*
+		translation_x(link[0])*convert3x3to4x4(rotation_x(link[1]))
 		)
-def sym_denavit_hartenberg( link_list):
+def sym_denavit_hartenberg( link):
 	return (
-		sym_translation_z(link_list[0])*sy.Matrix(syms_convert3x3to4x4(sym_rotation_z(link_list[1])))*
-		sym_translation_x(link_list[2])*sy.Matrix(syms_convert3x3to4x4(sym_rotation_x(link_list[3])))
+		sym_translation_z(link[2])*sy.Matrix(syms_convert3x3to4x4(sym_rotation_z(link[3])))*
+		sym_translation_x(link[0])*sy.Matrix(syms_convert3x3to4x4(sym_rotation_x(link[1])))
 		)
 
+def get_A0n( link_list):
+	A0i = np.identity(4)
+	A0n = []
+	for link in link_list:
+		A0i = A0i*denavit_hartenberg( link)
+		A0n.append(A0i)
+	return A0n
 
+def sym_get_A0n( link_list):
+	A0i = np.identity(4)
+	A0n = []
+	for link in link_list:
+		A0i = A0i*sym_denavit_hartenberg( link)
+		A0n.append(A0i)
+	return A0n
+
+
+def jacobian( link_list):
+	A0n = get_A0n( link_list)
+	# rotational vectors
+	R = [np.matrix(np.identity(3))]
+	R = R + [end[:3,:3] for end in A0n]
+	# positions of each end effector
+	O = [sy.Matrix([[0],[0],[0]])]
+	O = O + [np.matrix(end[:,3][:3]) for end in A0n]
+	# unit z vector
+	k = np.matrix([[0],[0],[1]])
+	J_v = []
+	J_w = []
+	for i in range(len(A0n)):
+		# if theta_i is 0 then joint is prismatic
+		if (link_list[i][3] == 0):
+			print np.shape(sy.Matrix(R[i]*k))
+			print sy.Matrix(R[i]*k)
+			print np.shape(sy.Matrix([[0],[0],[0]]))
+			J_v.append(sy.Matrix(R[i]*k))
+			J_w.append(sy.Matrix([[0],[0],[0]]))
+		# if theta_i is not 0 then joint is revolute
+		else:
+			J_v.append( np.matrix(np.cross( (R[i]*k).T, (O[-1]-O[i]).T)).T )
+			J_w.append(np.matrix(R[i]*k))
+	J = [np.vstack( (J_v[i], J_w[i])) for i in range(len(J_v))]
+	J = np.hstack(J)
+	return J
+
+
+def symbolic_jacobian( link_list):
+	A0n = sym_get_A0n( link_list)
+	# rotational vectors
+	R = [sy.Matrix(np.identity(3))]
+	R = R + [end[:3,:3] for end in A0n]
+	# positions of each end effector
+	O = [sy.Matrix([[0],[0],[0]])]
+	O = O + [sy.Matrix(end[:,3][:3]) for end in A0n]
+	# unit z vector
+	k = sy.Matrix([[0],[0],[1]])
+	J_v = []
+	J_w = []
+	for i in range(len(A0n)):
+		# if theta_i is 0 then joint is prismatic
+		if (link_list[i][3] == 0):
+			J_v.append(sy.Matrix(R[i]*k))
+			J_w.append(sy.Matrix([[0],[0],[0]]))
+		# if theta_i is not 0 then joint is revolute
+		else:
+			J_v.append((sy.Matrix(R[i]*k).cross(O[-1]-O[i])))
+			J_w.append(sy.Matrix(R[i]*k))
+	J = [sy.Matrix.vstack( J_v[i], J_w[i]) for i in range(len(J_v))]
+	J = sy.Matrix.hstack(J)
+	return J
+
+
+#def rotational_velocity_jacobian( link_list)
+#def jacobian(link_list)
